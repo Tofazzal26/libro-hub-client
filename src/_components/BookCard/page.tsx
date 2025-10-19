624f30e18aab261c6618c8cac24f4823b4c96846
Tofazzal26 <mdtofazzalislam6789@gmail.com>
2025-10-19 14:06:52 +0600

    book card dynamic data show finish

--- src/_components/BookCard/page.tsx
+++ src/_components/BookCard/page.tsx
@@ -1,30 +1,42 @@
-import { BookPlus, Eraser, NotebookPen, Trash } from "lucide-react";
+import { Book } from "@/redux/features/booksApi";
+import { BookPlus, Eraser, Trash } from "lucide-react";
 import Image from "next/image";
 import React from "react";
 
-const BookCard = () => {
+interface BookCardProps {
+  book: Book;
+}
+
+const BookCard: React.FC<BookCardProps> = ({ book }) => {
   return (
-    <div className="w-[400px]">
+    <div className="lg:px-0 px-2">
       <div className="border-[1px] border-gray-300 rounded-md p-4">
         <div>
           <div>
-            <div className="bg-[#f5f5f5] rounded-md p-14 relative">
-              <Image src={"/book11.png"} width={152} height={210} alt="" />
+            <div className="bg-[#ebecf0] rounded-md p-14 relative flex items-center justify-center flex-col">
+              <Image
+                src={book?.image}
+                width={152}
+                height={210}
+                alt=""
+                className="w-full"
+              />
               <span className="bg-gray-200 px-2 py-[6px] text-black absolute left-0 bottom-4 text-[12px] rounded-sm">
-                Copies: 20
+                Copies: {book?.copies}
               </span>
               <span className="bg-gray-200 px-2 py-[6px] text-black absolute right-0 bottom-4 text-[12px] rounded-sm">
-                In-Stock
+                {book?.availability === true ? "In-Stock" : "Out-Stock"}
               </span>
             </div>
           </div>
           <div>
             <div className="flex items-center justify-between mt-6">
-              <h2 className="text-[15px] text-[#64677d]">Tofazzal Hossain</h2>
-              <h2 className="text-[14px] text-[#64677d]">ISBN: 23235434</h2>
+              <h2 className="text-[15px] text-[#64677d]">{book?.author}</h2>
+              <h2 className="text-[14px] text-[#64677d]">ISBN: {book?.isbn}</h2>
             </div>
-            <h2 className="text-[#64677d] my-2">Genre: Self-Help</h2>
-            <p className="text-lg text-[#012e4a] ">The Hidden Mystery Behind</p>
+            <h2 className="text-[#64677d] my-2">Genre: {book?.genre}</h2>
+            <p className="text-lg text-[#012e4a] ">{book?.title}</p>
+            <p className="text-[#64677d] my-2 text-sm">{book?.description}</p>
             <div className="flex items-center justify-between mt-4">
               <button className="bg-[#b8393a] flex items-center gap-1 px-2 py-[6px] rounded-sm text-white text-sm">
                 Delete <Trash className="text-white" size={14} />
--- src/app/api/all-book/page.tsx
+++ src/app/api/all-book/page.tsx
@@ -4,7 +4,7 @@ import { useGetBooksQuery } from "@/redux/features/booksApi";
 import React from "react";
 
 const AllBook = () => {
-  const { data: books, error, isLoading } = useGetBooksQuery();
+  const { data: books, isLoading } = useGetBooksQuery();
 
   console.log(books);
 
@@ -12,9 +12,17 @@ const AllBook = () => {
     <div className="container mx-auto">
       <div className="my-10">
         <div>
-          <div>
-            <BookCard />
-          </div>
+          {isLoading ? (
+            <div className="flex justify-center items-center my-4 lg:my-8">
+              <div className="w-8 h-8 border-4 border-dashed rounded-full animate-spin border-[#b8393a]"></div>
+            </div>
+          ) : (
+            <div className="grid lg:grid-cols-4 md:grid-cols-2 grid-cols-1 lg:gap-4 gap-2">
+              {books?.map((book, idx) => (
+                <BookCard key={idx} book={book} />
+              ))}
+            </div>
+          )}
         </div>
       </div>
     </div>
