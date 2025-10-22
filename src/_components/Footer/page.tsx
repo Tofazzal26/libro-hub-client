1aaca49da1d299c740fd611848846e47c6f17fd2
Tofazzal26 <mdtofazzalislam6789@gmail.com>
2025-10-22 12:24:55 +0600

    footer brand name change

--- src/_components/Footer/page.tsx
+++ src/_components/Footer/page.tsx
@@ -17,7 +17,7 @@ const Footer = () => {
                     src="/logo.png"
                     alt="MerakiUI Logo"
                   />
-                  <h2 className="text-lg text-white">Librohub</h2>
+                  <h2 className="text-lg text-gray-700">Librohub</h2>
                 </div>
 
                 <p className="max-w-sm mt-2 text-gray-500 dark:text-gray-400">
